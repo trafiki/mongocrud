4e621ca33a4a923d22164bf78b1ff786e084d91a
Akerele Tunde <babsake@gmail.com>
2016-03-08 19:41:30 +0100

    A functionality which allow users to choose how they want to crawl got added.

--- crawler.go
+++ crawler.go
@@ -6,87 +6,126 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"os"
+	"strings"
+	"time"
 
-	"github.com/jackdanger/collectlinks" // The collectlinks library is made for parsing links
+	"github.com/jackdanger/collectlinks" // The collectlinks library is made for parsing links.
 )
 
+var transport = &http.Transport{
+	TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true,
+	},
+}
+
+var client = http.Client{Transport: transport}
+
+var deadlinks = []error{}
+
 func main() {
+	target := flag.String("url", "", "Please supply a url")
+	crawlAll := flag.Bool("fullcrawl", true, "Option to crawl the domain only")
 	flag.Parse()
 
-	args := flag.Args()
-	fmt.Println(args)
-	if len(args) < 1 {
-		fmt.Println("Please specify start page")
-		os.Exit(1)
+	fmt.Println("Crawling Address: ", *target)
+
+	targetURI, err := url.Parse(*target)
+	if err != nil {
+		fmt.Printf("Invalid Address[%s]\n", *target)
+		return
 	}
 
 	queue := make(chan string)
 	filteredQueue := make(chan string)
 
-	// This go routine sends the arg supplied at the command line to the queue channel.
-	go func() { queue <- args[0] }()
-	go filterQueue(queue, filteredQueue)
-
-	// pull from the filtered queue, add to the unfiltered queue
-	for uri := range filteredQueue {
-		enqueue(uri, queue)
-	}
-}
-
-// filterQueue makes sure a link is not crawled twice
-func filterQueue(in chan string, out chan string) {
 	var seen = make(map[string]bool)
-	for val := range in {
-		if !seen[val] {
-			seen[val] = true
-			out <- val
+
+	// This go routine sends the target url supplied at the command line to the queue channel.
+	go func() { queue <- *target }()
+
+	for {
+		select {
+		case val, ok := <-queue:
+			if !ok {
+				return
+			}
+			if !seen[val] {
+				seen[val] = true
+				go func() { filteredQueue <- val }()
+				continue
+			}
+
+		case currentURI, ok := <-filteredQueue:
+			if !ok {
+				return
+			}
+
+			enqueue(currentURI, queue, targetURI.Host, *crawlAll)
+
+		case <-time.After(1 * time.Minute):
+			fmt.Printf("Expired beyond request timeout.\n")
+			fmt.Println("---------------Dead Links-----------------------")
+			for _, i := range deadlinks {
+				fmt.Println(i)
+			}
+			fmt.Println("------------------------------------------------")
+			return
 		}
 	}
+
 }
 
 // enqueue retrieves HTTP and parses the links, putting them into the same queue
-// used by main
-func enqueue(uri string, queue chan string) {
-	fmt.Println("fetching", uri)
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
+// used by main.
+func enqueue(uri string, queue chan string, targetHost string, crawlAll bool) {
+	fmt.Println("Fetching: ", uri)
 
-	client := http.Client{Transport: transport}
-	resp, err := client.Get(uri)
+	currentURI, err := url.Parse(uri)
 	if err != nil {
+
 		return
 	}
+
+	if crawlAll && !strings.Contains(currentURI.Host, targetHost) {
+		return
+	}
+
+	resp, err := client.Get(uri)
+	if err != nil {
+		fmt.Println("")
+		fmt.Printf("Host: %s\n", currentURI.Host)
+		fmt.Printf("URL: %s\n", currentURI.String())
+		fmt.Printf("Error: %s\n", err.Error())
+		fmt.Println("")
+		deadlinks = append(deadlinks, err)
+		return
+	}
+
 	defer resp.Body.Close()
 
 	links := collectlinks.All(resp.Body)
 
 	for _, link := range links {
-		absolute := fixUrl(link, uri)
-		resp2, err := client.Get(absolute)
+		fixedLink, err := fixURL(link, uri)
 		if err != nil {
-			return
+			fmt.Println(err)
+			continue
 		}
-		if uri != "" && resp2.StatusCode == 404 {
-			fmt.Println(absolute, "is a dead link")
-			go func() { queue <- absolute }()
-		}
+
+		queue <- fixedLink
 	}
 }
 
-// fixUrl makes sure that the link being visited is a relative link
-func fixUrl(href, base string) string {
+func fixURL(href, base string) (string, error) {
 	uri, err := url.Parse(href)
 	if err != nil {
-		return ""
+		return "", err
 	}
-	baseUrl, err := url.Parse(base)
+
+	baseURL, err := url.Parse(base)
 	if err != nil {
-		return ""
+		return "", err
 	}
-	uri = baseUrl.ResolveReference(uri)
-	return uri.String()
+
+	return baseURL.ResolveReference(uri).String(), nil
 }
