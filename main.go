3047968e6cf9966b84afcb79eb5331f77f7320a4
Akerele Tunde <babsake@gmail.com>
2016-03-31 21:08:45 +0100

    Made adjustments according to the reviews.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -8,14 +9,6 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-//==============================================================================
-// If you are connecting to a mgo database, you don't need to always redial
-// else you are creating multiple connections, a mongo session can be used
-// multiple times as many as you want. Hence I will only create it once then
-// get the session to do what i want in the other functions.
-
-// ses provides internal global session for our database operation,we will
-// create new sessions of this one, once we have successfully connected.
 var ses *mgo.Session
 
 func getSession() *mgo.Session {
@@ -32,13 +25,12 @@ func getSession() *mgo.Session {
 		panic(err)
 	}
 
-	// We are setting the mod of operation to Monotonic to ensure we get
+	// We are setting the mode of operation to Monotonic to ensure we get
 	// assurances that our writes and reads are synchornized and we not
 	// cause inconsistencies.
-	// NOTE: Never forget to do this.
 	session.SetMode(mgo.Monotonic, true)
 
-	ses = session
+	ses = session.Copy()
 	return session
 }
 
@@ -52,42 +44,29 @@ type Profile struct {
 	LastUpdated time.Time
 }
 
-// NOTE:Question, if why do I want to declare this outside, do they
-// have a relevance to not being declared within the main() function,
-// Is your plan to provide these as global profiles forever?
-// TODO: move them into your main function, else describe the reason why
-// you have them here, it better be good, constructive and have a really
-// solid reasoning behind it. O_O
-var tunde = &Profile{
-	Name:        "tunde",
-	Password:    "akerele",
-	Age:         27,
-	LastUpdated: time.Now(),
-}
-
-var saheed = &Profile{
-	Name:        "saheed",
-	Password:    "ajibulu",
-	Age:         27,
-	LastUpdated: time.Now(),
-}
-
 func main() {
-	// Adding a profile to the database
-	tunde.CreateOrUpdateProfile()
-	saheed.CreateOrUpdateProfile()
+
+	// Adding profiles to the database
+	CreateProfile("Tom", "tominson", 27)
+	CreateProfile("Dick", "dickinson", 27)
+	CreateProfile("Harry", "harrison", 27)
 
 	// Get all profiles in the Profiles collection.
 	GetProfiles()
 
 	// Get a profille from the Profiles collection.
-	ReadProfile(tunde.Name)
+	ReadProfile("Tom")
 
-	// TODO: WHAT happend to the UD (Update Delete) part of CRUD O_O?
+	// Delete a profile from the profiles collection.
+	DeleteProfile("Dick")
+
+	// Update a profile in the profiles collection.
+	UpdateProfile("Harry", "harry", 25)
+
 }
 
 // GetProfiles returns all the profile in the Profiles Collection
-func GetProfiles() []Profile {
+func GetProfiles() *[]Profile {
 	// Get a session.
 	session := getSession()
 	defer session.Close()
@@ -95,40 +74,33 @@ func GetProfiles() []Profile {
 	c := session.DB("ProfileService").C("Profiles")
 
 	var profiles []Profile
-	// NOTE: Indent your code
-	// TODO: Do you really think not checking your error is appropriate here, if am running this
-	// on production server, my whole app will have a fault because i dont even know
-	// if i found a valid record.
-	// Those that make sense to you? O_O
-	err = c.Find(bson.M{}).All(&profiles)
 
-	return profiles
+	err := c.Find(bson.M{}).All(&profiles)
+	if err != nil {
+		log.Println("Error getting profiles: ", err.Error())
+		return &profiles
+	}
+
+	return &profiles
 }
 
 // ReadProfile returns the profile in the Profiles Collection with name equal to the id parameter (id == name)
-func ReadProfile(id string) Profile {
+func ReadProfile(id string) *Profile {
 	// Get a session.
 	session := getSession()
 	defer session.Close()
 
 	c := session.DB("ProfileService").C("Profiles")
-	// NOTE:What happened to property indented code?
-	// NOTE: why not do a:   "var profile Profile".
-	// When declaring variables you do plan to initialize with a value
-	// use the declaration not the assignative form.
-	profile := Profile{}
 
-	// TODO: Do you really think not checking your error is appropriate here, if am running this
-	// on production server, my whole app will have a fault because i dont even know
-	// if i found a valid record.
-	// Those that make sense to you? O_O
-	err = c.Find(bson.M{"name": id}).One(&profile)
+	var profile Profile
 
-	// NOTE: use pointers when returning something, why do i want to have a copy?
-	// What if i want to change something?
-	// Do i really want a copy of the profile and not a reference here
-	// Does having a copy make sense O_O to having a reference?
-	return profile
+	err := c.Find(bson.M{"name": id}).One(&profile)
+	if err != nil {
+		log.Println("Error reading profile: ", err.Error())
+		return &profile
+	}
+
+	return &profile
 }
 
 // DeleteProfile deletes the profile in the Profiles Collection with name equal to the id parameter (id == name)
@@ -138,31 +110,61 @@ func DeleteProfile(id string) bool {
 	defer session.Close()
 
 	c := session.DB("ProfileService").C("Profiles")
-	// TODO: Do you really think not checking your error is appropriate here, if am running this
-	// on production server, my whole app will have a fault because i dont even know
-	// if i found a valid record.
-	// Those that make sense to you? O_O
-	err = c.RemoveId(id)
+
+	err := c.Remove(bson.M{"name": id})
+	if err != nil {
+		log.Println("Error removing profile: ", err.Error())
+		return false
+	}
+
+	fmt.Println(id, "has been removed.")
 
 	return true
 }
 
-// NOTE: not that it is wrong, but why not have a function that you pass a
-// Profile to, to add it into the db. but you can peek and choose as you like.
-// CreateOrUpdateProfile Creates or Updates the profile in the Profiles Collection with id parameter
-func (p *Profile) CreateOrUpdateProfile() bool {
+// CreateProfile creates a profile for the Profiles collection.
+func CreateProfile(id string, password string, age int) bool {
 	// Get a session.
 	session := getSession()
 	defer session.Close()
 
 	c := session.DB("ProfileService").C("Profiles")
-	// NOTE: Indent your code
-	_, err = c.UpsertId(p.Name, p)
-	// NOTE: Indent your code
+
+	index := mgo.Index{
+		Key:    []string{"name"},
+		Unique: true,
+	}
+
+	err := c.EnsureIndex(index)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	err = c.Insert(&Profile{id, password, age, time.Now()})
+
 	if err != nil {
 		log.Println("Error creating Profile: ", err.Error())
 		return false
 	}
-	// NOTE: Indent your code
+
+	return true
+}
+
+// UpdateProfile updates a profile in the profiles collection with the id parameter.
+func UpdateProfile(id string, password string, age int) bool {
+	session := getSession()
+	defer session.Close()
+
+	c := session.DB("ProfileService").C("Profiles")
+
+	querier := bson.M{"name": id}
+	change := bson.M{"$set": bson.M{"Password": password, "Age": age, "LastUpdated": time.Now()}}
+
+	err := c.Update(querier, change)
+	if err != nil {
+		log.Println("Error updating profile: ", err.Error())
+		return false
+	}
+
 	return true
 }
